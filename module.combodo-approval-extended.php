e0f78b9ea72d2e1c3e076860699fcf018958769d
v-dumas <vincent.dumas@combodo.com>
2026-08-20 18:24:23 +0200

    🔖 Update module version to 1.5.4

--- module.combodo-approval-extended.php
+++ module.combodo-approval-extended.php
@@ -17,7 +17,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-approval-extended/1.5.3',
+	'combodo-approval-extended/1.5.4',
 	[
 		// Identification
 		//
